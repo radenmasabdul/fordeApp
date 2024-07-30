629fca33bf65fc00e9d0c5bd42f58e4433975138
radenmasabdul <abdul.tamsis@gmail.com>
2024-07-30 17:46:43 +0700

    update in navbar

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -1,26 +1,52 @@
 <script setup>
 import Logo from "../../assets/logo.jpg";
+
+import { useRoute } from "vue-router";
+import { computed } from "vue";
+
+const route = useRoute();
+
+const pageTitle = computed(() => {
+  switch (route.name) {
+    case "home":
+      return "Dashboard";
+    case "employees":
+      return "Employee Details";
+  }
+});
+
+let classStyle =
+  "font-JakartaSans font-semibold text-base capitalize block bg-#e0e0e0 w-full border border-slate-300 rounded-md py-2 px-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1";
 </script>
 
 <template>
   <div class="navbar bg-base-200">
     <div class="flex-1">
-      <a class="font-JakartaSans font-bold text-2xl">AL Company</a>
+      <a class="font-JakartaSans font-bold text-2xl">{{ pageTitle }}</a>
     </div>
     <div class="flex-none gap-2">
+      <select name="filter" id="filter" :class="classStyle">
+        <option>Filter</option>
+        <option value="1">EN</option>
+        <option value="2">ID</option>
+      </select>
+
       <button class="btn btn-ghost btn-circle">
-        <div class="indicator">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-            />
-          </svg>
-          <span class="badge badge-xs badge-primary indicator-item"></span>
-        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          />
+        </svg>
       </button>
+
+      <select name="lang" id="lang" :class="classStyle">
+        <option value="EN">EN</option>
+        <option value="ID">ID</option>
+      </select>
+
       <div class="dropdown dropdown-end">
         <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
           <div class="w-10 rounded-full">
@@ -38,6 +64,19 @@ import Logo from "../../assets/logo.jpg";
           <li><a>Logout</a></li>
         </ul>
       </div>
+      <button class="btn btn-ghost btn-circle">
+        <div class="indicator">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+            />
+          </svg>
+          <span class="badge badge-xs badge-primary indicator-item"></span>
+        </div>
+      </button>
     </div>
   </div>
 </template>
