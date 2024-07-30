79fe6d0d7d326715942b83dce242816be4c62632
radenmasabdul <abdul.tamsis@gmail.com>
2024-07-30 14:13:01 +0700

    Inisialisasi

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import './styles/style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
+library.add(fas)
+
+const pinia = createPinia()
+const app = createApp(App)
+
+app.component("font-awesome-icon", FontAwesomeIcon)
+app.use(pinia)
+app.mount('#app')
+
